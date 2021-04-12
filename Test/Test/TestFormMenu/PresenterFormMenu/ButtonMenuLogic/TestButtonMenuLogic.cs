c5a6616d89d4ec5c1696362e3cc44ce73ff525de
Artur.Kaczmarczyk89@gmail.com <artur.kaczmarczyk89@gmail.com>
2021-04-12 19:03:39 +0200

    przebudowa TestButtonMenuLogic

--- Test/Test/TestFormMenu/PresenterFormMenu/ButtonMenuLogic/TestButtonMenuLogic.cs
+++ Test/Test/TestFormMenu/PresenterFormMenu/ButtonMenuLogic/TestButtonMenuLogic.cs
@@ -15,24 +15,27 @@ namespace Test.Test.TestFormMenu.PresenterFormMenu.ButtonMenuLogic
         [TestCase( "Vegetariana", "22zł", "1" )]
         [TestCase( "Tosca", "25zł", "2" )]
         [TestCase( "Venecia", "25zł", "3" )]
-        public void TestGetListPizzasFromListViewDishes( string name, string price, int index )
+        public void TestGetListPizzasFromListViewDishes( string expectationsName, string expectationsPrice, int index )
         {
             eevent.SetLogic( new ButtonPizzaLogic( form ) );
+            var  currentName  = form.ListViewDishes.Items [index].SubItems [0].Text;
+            var  currentPrice = form.ListViewDishes.Items [index].SubItems [1].Text;
 
-            Assert.AreEqual( name, form.ListViewDishes.Items [index].SubItems [0].Text );
-            Assert.AreEqual( price, form.ListViewDishes.Items [index].SubItems [1].Text );
+            Assert.AreEqual( expectationsName, currentName );
+            Assert.AreEqual( expectationsPrice, currentPrice );
         }
-
 
         [TestCase( "Schabowy z frytkami/ryżem/ziemniakami", "30zł", "0" )]
         [TestCase( "Ryba z frytkami", "28zł", "1" )]
         [TestCase( "Placek po węgiersku", "27zł", "2" )]
-        public void TestGetListMainDishesFromListViewDishes( string name, string price, int index )
+        public void TestGetListMainDishesFromListViewDishes( string expectationsName, string price, int index )
         {
             eevent.SetLogic( new ButtonMainDishesLogic( form ) );
+            var  currentName  = form.ListViewDishes.Items [index].SubItems [0].Text;
+            var  currentPrice = form.ListViewDishes.Items [index].SubItems [1].Text;
 
-            Assert.AreEqual( name, form.ListViewDishes.Items [index].SubItems [0].Text );
-            Assert.AreEqual( price, form.ListViewDishes.Items [index].SubItems [1].Text );
+            Assert.AreEqual( expectationsName, currentName );
+            Assert.AreEqual( price, currentPrice );
         }
 
         [TestCase( "Kawa", "0" )]
@@ -41,39 +44,45 @@ namespace Test.Test.TestFormMenu.PresenterFormMenu.ButtonMenuLogic
         public void TestGetListDrinksFromListViewDishes( string expectationsName, int index )
         {
             eevent.SetLogic( new ButtonDriksLogic( form ) );
+            var  currentName  = form.ListViewDishes.Items [index].SubItems [0].Text;
+            var  currentPrice = form.ListViewDishes.Items [index].SubItems [1].Text;
 
-            Assert.AreEqual( expectationsName, form.ListViewDishes.Items [index].SubItems [0].Text );
-            Assert.AreEqual( "5zł", form.ListViewDishes.Items [index].SubItems [1].Text );
+            Assert.AreEqual( expectationsName, currentName );
+            Assert.AreEqual( "5zł", currentPrice );
         }
 
         [TestCase( "Pomidorowa", "12zł", "0" )]
         [TestCase( "Rosół", "10zł", "1" )]
-        public void TestGetListSoupsFromListViewDishes( string name, string price, int index )
+        public void TestGetListSoupsFromListViewDishes( string expectationsName, string expectationsPrice, int index )
         {
             eevent.SetLogic( new ButtonSoupsLogic( form ) );
+            var  currentName  = form.ListViewDishes.Items [index].SubItems [0].Text;
+            var  currentPrice = form.ListViewDishes.Items [index].SubItems [1].Text;
 
-            Assert.AreEqual( name, form.ListViewDishes.Items [index].SubItems [0].Text );
-            Assert.AreEqual( price, form.ListViewDishes.Items [index].SubItems [1].Text );
+            Assert.AreEqual( expectationsName, currentName );
+            Assert.AreEqual( expectationsPrice, currentPrice );
         }
 
         [TestCase( "Bar sałatkowy -5zł", "0" )]
         [TestCase( "Zestaw sosów -6zł", "1" )]
-        public void TestGetListSidesFromMainDishes( string name, int index )
+        public void TestGetListSidesFromMainDishes( string expectationsName, int index )
         {
             eevent.SetLogic( new ButtonMainDishesLogic( form ) );
+            var  currentName  = form.CheckedListBoxSide.Items [index].ToString();
 
-            Assert.AreEqual( name, form.CheckedListBoxSide.Items [index].ToString() );
+            Assert.AreEqual( expectationsName, currentName );
         }
 
         [TestCase( "Podwójny Ser -2zł", "0" )]
         [TestCase( "Salami -2zł", "1" )]
         [TestCase( "Szynka -2zł", "2" )]
         [TestCase( "Pieczarki -2zł", "3" )]
-        public void TestGetListSidesFromPizzas( string name, int index )
+        public void TestGetListSidesFromPizzas( string expectationsName, int index )
         {
             eevent.SetLogic( new ButtonPizzaLogic( form ) );
+            var  currentName  = form.CheckedListBoxSide.Items [index].ToString();
 
-            Assert.AreEqual( name, form.CheckedListBoxSide.Items [index].ToString() );
+            Assert.AreEqual( expectationsName, currentName );
         }
     }
 }

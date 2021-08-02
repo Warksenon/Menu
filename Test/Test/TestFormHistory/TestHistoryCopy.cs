cb699e06e3eafb38359448a5b9b5b4345605d34b
Warksenon <artur.kaczmarczyk89@gmail.com>
2021-08-02 20:58:52 +0200

    add test class HistoryListViewDishes

--- Test/Test/TestFormHistory/TestHistoryCopy.cs
+++ Test/Test/TestFormHistory/TestHistoryCopy.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+
 using NUnit.Framework;
+
 using Pizza;
 using Pizza.Presenters.PresenterFormHistory.CopyHistory;
 
